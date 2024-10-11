28f7d84471938609cbd662cac7559ead1abe8aba
Akrm Al-Hakimi <alhakimiakrmj@gmail.com>
2024-10-10 23:59:57 -0400

    redoing level editor

---
+++ src/LevelEditor.cpp
@@ -0,0 +1,6 @@
+#include "../include/LevelEditor.hpp"
+#include <fstream>
+#include <cmath>
+#include <iostream>
+
+LevelEditor::LevelEditor(float gridSize, const std::vector<sf::Texture>& textures)
